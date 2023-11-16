4d01c98e9c2e8ad547d12f88d1b7f6bb0e938ac7
Isak Martin Simbolon <simbolonmartin22@gmail.com>
2023-11-16 17:28:25 +0800

    add multiple positions

--- wrist_motor_position_control.py
+++ wrist_motor_position_control.py
@@ -41,31 +41,41 @@ class MotorCommunication():
 
     def test_send_message_position(self):
         internalJog         = [0x01, 0x06, 0x05, 0x12, 0x00, 0x21, 0xE9, 0x1B]
-        setControlSpeed     = [0x01, 0x06, 0x02, 0x01, 0x00, 0XC8, 0xD8, 0x24]
+        setControlSpeed     = [0x01, 0x06, 0x02, 0x01, 0x00, 0XC8, 0xD8, 0x24] #200rpm
         stopInternalJog     = [0x01, 0x06, 0x05, 0x12, 0x00, 0x01, 0xE8, 0xC3]
         controlHomingStart  = [0x01, 0x06, 0x05, 0x12, 0x02, 0x01, 0xE9, 0xA3]
         monitorHoming       = [0x01, 0x03, 0x06, 0x1E, 0x00, 0x01, 0xE4, 0x84]
         closeHoming         = [0x01, 0x06, 0x05, 0x12, 0x00, 0x01, 0xE8, 0xC3]
-        setPosition         = [0x01, 0x10, 0x07, 0x01, 0x00, 0x04, 0x08, 0x00, 0x00, 0x03, 0xE8, 0x00, 0x00, 0x00, 0x0A, 0xA0, 0xDD] #
-        # setPosition2        = [0x01, 0x10, 0x07, 0x01, 0x00, 0x04, 0x08, 0x00, 0x01, 0x3E, 0x80, 0x00, 0x00, 0x00, 0x64, 0x54, 0xD5]
-        triggerMovement     = [0x01, 0x06, 0x05, 0x12, 0x00, 0x81, 0xE9, 0x63]
-        move1               = [0x01, 0x06, 0x05, 0x12, 0x00, 0x01, 0xE8, 0xC3]
-        move2               = [0x01, 0x06, 0x05, 0x12, 0x00, 0x81, 0xE9, 0X63]
 
+        #move with target position = 0 rev and 1000 pulse
+        setPosition         = [0x01, 0x10, 0x07, 0x01, 0x00, 0x04, 0x08, 0x00, 0x00, 0x03, 0xE8, 0x00, 0x00, 0x00, 0x0A, 0xA0, 0xDD] 
+        #move with target position = 1 rev and 1000 pulse
+        setPosition2        = [0x01, 0x10, 0x07, 0x01, 0x00, 0x04, 0x08, 0x00, 0x01, 0x03, 0xE8, 0x00, 0x00, 0x00, 0x64, 0x54, 0xD5]
+        #move with target position = 10 rev and 0 pulse
+        setPosition3        = [0x01, 0x10, 0x07, 0x01, 0x00, 0x04, 0x08, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x64, 0xEB, 0x15]
+
+        
+        triggerMovement     = [0x01, 0x06, 0x05, 0x12, 0x00, 0x81, 0xE9, 0x63]
+        stop_movement       = [0x01, 0x06, 0x05, 0x12, 0x00, 0x01, 0xE8, 0xC3]
+ 
         # print("internalJog")
         # res = self.ser.write(internalJog)
         # self.read_response(8)
         # self.read_current_alarm()
-    
+        # time.sleep(2)
+
+
         # print("setControlSpeed")
         # res = self.ser.write(setControlSpeed)
         # self.read_response(8)
         # self.read_current_alarm()
-        
+        # time.sleep(2)
+
         # print("stopInternalJog")
         # res = self.ser.write(stopInternalJog)
         # self.read_response(8)
         # self.read_current_alarm()
+        # time.sleep(3)
 
         # print("controlHomingStart")
         # res = self.ser.write(controlHomingStart)
@@ -74,34 +84,31 @@ class MotorCommunication():
         # time.sleep(10)
 
         # print("monitorHoming")
-        # # while True:
         # res = self.ser.write(monitorHoming)
         # self.read_response(7)
-        # # time.sleep(3)
-        # # res = self.ser.write(monitorHoming)
-        # # self.read_response(7)
-        
-
-
+        # self.read_current_alarm()
+        # time.sleep(1)
+    
         # print("closeHoming")
         # res = self.ser.write(closeHoming)
         # self.read_response(8)
         # self.read_current_alarm()
+        # time.sleep(1)
 
-        print("setPosition")
-        res = self.ser.write(setPosition)
-        # self.read_response(8)
-        # self.read_current_alarm()
-        time.sleep(0.1)
-
-        print("triggerMovement")
-        res = self.ser.write(triggerMovement)
+        # print("setPosition")
+        # res = self.ser.write(setPosition)
         # self.read_response(8)
         # self.read_current_alarm()
         # time.sleep(0.1)
 
-        print("move1")
-        res = self.ser.write(move1)
+        # print("triggerMovement")
+        # res = self.ser.write(triggerMovement)
+        # self.read_response(8)
+        # self.read_current_alarm()
+        # time.sleep(5)
+
+        # print("stop_movement")
+        # res = self.ser.write(stop_movement)
         # self.read_response(8)
         # time.sleep(2)
         # self.read_current_alarm()
@@ -112,14 +119,52 @@ class MotorCommunication():
         # self.read_current_alarm()
         # time.sleep(0.1)
 
+        # print("triggerMovement")
+        # res = self.ser.write(triggerMovement)
+        # self.read_response(8)
+        # self.read_current_alarm()
+        # time.sleep(3)
 
-
-        # print("move2")
-        # res = self.ser.write(move2)
+        # print("stop_movement")
+        # res = self.ser.write(stop_movement)
         # self.read_response(8)
         # time.sleep(2)
         # self.read_current_alarm()
 
+        print("setPosition3")
+        res = self.ser.write(setPosition3)
+        self.read_response(8)
+        self.read_current_alarm()
+        time.sleep(0.1)
+
+        print("triggerMovement")
+        res = self.ser.write(triggerMovement)
+        self.read_response(8)
+        self.read_current_alarm()
+        time.sleep(13)
+
+        print("stop_movement")
+        res = self.ser.write(stop_movement)
+        self.read_response(8)
+        time.sleep(2)
+        self.read_current_alarm()
+        
+    def test_send_speed(self):
+        speed200rpm = [0x01, 0x06, 0x02, 0x01, 0x00, 0xC8, 0xD8, 0x24]
+        speed0rpm = [0x01, 0x06, 0x02, 0x01, 0x00, 0x00, 0xD9, 0xB2]
+
+        print("TEST SEND SPEED")
+        print("speed200rpm")
+        res = self.ser.write(speed200rpm)
+        self.read_response(8)
+        self.read_current_alarm()
+
+        # time.sleep(3)
+
+        print("speed0rpm")
+        res = self.ser.write(speed0rpm)
+        self.read_response(8)
+        self.read_current_alarm()
 
     def read_current_alarm(self):
         readCurrentAlarm    = [0x01, 0x03, 0x06, 0x3F, 0x00, 0x01, 0xB4, 0x8E]
@@ -156,7 +201,7 @@ if __name__ == "__main__":
     handle = MotorCommunication()
     handle.check_conn()
     handle.initialize_driver()
-    # handle.initialize_driver()
-
     handle.test_send_message_position()
+    # handle.test_send_speed()
+
     print("Finished")
